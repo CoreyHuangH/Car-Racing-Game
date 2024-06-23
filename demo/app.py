66055aa3e510038ba5d0623b2943873c72c7bf4a
UNNC_Corey.H <corey.huang0103@gmail.com>
2024-06-23 14:55:38 +0800

    Upload

--- demo/app.py
+++ demo/app.py
@@ -1,13 +1,5 @@
 import gradio as gr
-
-
-def show_videos():
-    # Paths to the video files
-    final_model_video = "./rendered_videos/car_racing_final_model.avi"
-    best_model_video = "./rendered_videos/car_racing_best_model.avi"
-
-    return final_model_video, best_model_video
-
+from video_viewer import show_videos
 
 # Define the Gradio interface
 interface = gr.Interface(
---
+++ demo/video_viewer.py
@@ -0,0 +1,6 @@
+def show_videos():
+    # Paths to the video files
+    final_model_video = "./rendered_videos/car_racing_final_model.avi"
+    best_model_video = "./rendered_videos/car_racing_best_model.avi"
+
+    return final_model_video, best_model_video
