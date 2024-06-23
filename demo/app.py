352014cac4f346a264d4df3e1598bf60c04e952f
UNNC_Corey.H <corey.huang0103@gmail.com>
2024-06-23 22:43:07 +0800

    Reformat

--- demo/app.py
+++ demo/app.py
@@ -1,4 +1,5 @@
 import gradio as gr
+
 from viewer import show_videos_and_scores
 
 # Define the Gradio interface
@@ -6,9 +7,15 @@ interface = gr.Interface(
     fn=show_videos_and_scores,
     inputs=[],
     outputs=[
-        gr.Video(label="Final Model Video", value="./rendered_videos/car_racing_final_model.avi"),
+        gr.Video(
+            label="Final Model Video",
+            value="./rendered_videos/car_racing_final_model.avi",
+        ),
         gr.Textbox(label="Final Model Score", value="902.5"),
-        gr.Video(label="Best Model Video", value="./rendered_videos/car_racing_best_model.avi"),
+        gr.Video(
+            label="Best Model Video",
+            value="./rendered_videos/car_racing_best_model.avi",
+        ),
         gr.Textbox(label="Best Model Score", value="919.9"),
     ],
     title="Car Racing Videos Demo",
