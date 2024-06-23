45b4e8ff7926c5118c844b2306a85fcdd2dd0303
UNNC_Corey.H <corey.huang0103@gmail.com>
2024-06-23 15:31:26 +0800

    Update viewer.py

--- demo/viewer.py
+++ demo/viewer.py
@@ -2,6 +2,7 @@ def show_videos_and_scores():
     # Paths to the video files
     final_model_video = "./rendered_videos/car_racing_final_model.avi"
     best_model_video = "./rendered_videos/car_racing_best_model.avi"
+    # Scores of the videos, this can be found in tensorboard logs
     final_model_score = 902.5
     best_model_score = 919.9
 
